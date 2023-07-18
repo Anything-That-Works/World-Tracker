60dfeaf690c7b30136e0be91ad246f640d30e7c7
Promal <promal.barua.cse@gmail.com>
2023-07-18 09:01:23 +0600

    Initial Commit

---
+++ World Tracker/World_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_TrackerApp.swift
+//  World Tracker
+//
+//  Created by Promal on 18/7/23.
+//
+
+import SwiftUI
+
+@main
+struct World_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
